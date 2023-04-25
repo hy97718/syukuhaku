aa46235ad77ab72ef5525c13994267d479a5c836
0718hyui@gmail.com <0718hyui@gmail.com>
2023-04-25 11:09:11 +0900

    ユーザーモデルvalidationの追加、users_controllerのupdateアクションの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,18 +33,31 @@ end
 
 def update
   @user =User.find(params[:id])
-  if @user.update(user_params)
-    sign_in :user, @user, bypass: true
-    redirect_to user_path(@user), notice: "プロフィール情報を更新しました。"
-  else
-    flash.now[:alert] = "プロフィール情報を更新できませんでした。"
-    render action: :show
+  if user_params.present?
+    if @user.update(user_params)
+      sign_in :user, @user, bypass: true
+      redirect_to user_path(@user), notice: "プロフィール情報を更新しました。"  
+    else
+      flash.now[:alert] = "プロフィール情報を更新できませんでした。"
+      render action: :edit
+    end  
+  elsif info_params.present?
+    if @user.update(info_params)
+      sign_in :user, @user, bypass: true
+      redirect_to user_path(@user), notice: "アカウント情報を更新しました。"  
+    else
+      flash.now[:alert] = "アカウント情報を更新できませんでした。"
+      render action: :acount_edit
+    end    
   end  
 end
 
 private
   def user_params
-    params.require(:user).permit(:username, :email, :profile, :image, :password )
+    params.require(:user).permit(:username, :profile, :image )
+  end
+  def info_params
+    params.require(:user).permit(:email, :password )
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
   has_one_attached :image
   
   validates :image,  blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'] }
-  
   validates :username, presence: true
-  
   validates :profile, length: { maximum: 50 }  
-  
+  validates :password, presence: true, length: { in: 6..30 }, on: :acount_update
+  validates :email, presence: true, uniqueness: true, on: :acount_update
 end
-
